1341579f2eb4801f12dade0d229d05541eeb451a
Ron Stephen Mathew @ Newtron <47325659+NewtronReal@users.noreply.github.com>
2024-11-02 23:04:46 +0530

    added mDNS vethramdevice.local

--- master/serialwriter.ino
+++ master/serialwriter.ino
@@ -1,14 +1,12 @@
 #include <WiFi.h>
-#include <NetworkClient.h>
-#include <WiFiAP.h>
 #include <WiFiUdp.h>
+#include <ESPmDNS.h>
 
 #define UDP_PORT 4444
 
 const char *ssid = "VDMAEQ720VD";
 const char *password = "something";
-
-NetworkServer server(80);
+const char *mdnsName = "vethramdevice";  // This will allow discovery as "esp32.local"
 
 WiFiUDP udp;
 
@@ -17,28 +15,42 @@ void setup() {
   Serial.println();
   Serial.println("Configuring access point...");
 
+  // Set up Wi-Fi access point
   if (!WiFi.softAP(ssid, password)) {
     log_e("Soft AP creation failed.");
     while (1);
   }
+
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  server.begin();
-  Serial.println("Server started");
+
+  // Start UDP server
   udp.begin(UDP_PORT);
-  Serial.print("Listenint at:");
+  Serial.print("Listening at: ");
   Serial.println(WiFi.softAPIP());
+
+  // Set up mDNS responder
+  if (!MDNS.begin(mdnsName)) {
+    Serial.println("Error setting up mDNS responder!");
+  } else {
+    Serial.print("mDNS responder started. Hostname: ");
+    Serial.println(mdnsName);
+    Serial.print("You can reach the ESP32 at: ");
+    Serial.print(mdnsName);
+    Serial.println(".local");
+  }
 }
 
 void loop() {
   uint8_t buffer[14];
-  memset(buffer,0,14);
-  udp.parsePacket();
-  if(udp.read(buffer,14)>0){
-    if(char(buffer[0]=='r')){
-
-    }
-    Serial.write(buffer,14);
+  memset(buffer, 0, 14);
+  
+  // Check if UDP data is available
+  int packetSize = udp.parsePacket();
+  if (packetSize > 0) {
+    // Read the incoming UDP data
+    udp.read(buffer, 14);
+    Serial.write(buffer, 14);  // Output the data for debugging
   }
 }
